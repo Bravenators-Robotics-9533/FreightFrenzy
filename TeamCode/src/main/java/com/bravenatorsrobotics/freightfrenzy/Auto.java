29789042316b161065964ed1727137fabe732448
Nick-Fanelli <nickfanelli5@gmail.com>
2021-11-23 20:26:23 -0500

    fix: red shipping hub autonomous

--- TeamCode/src/main/java/com/bravenatorsrobotics/freightfrenzy/Auto.java
+++ TeamCode/src/main/java/com/bravenatorsrobotics/freightfrenzy/Auto.java
@@ -3,18 +3,14 @@ package com.bravenatorsrobotics.freightfrenzy;
 import com.bravenatorsrobotics.common.drive.AbstractDrive;
 import com.bravenatorsrobotics.common.drive.MecanumDrive;
 import com.bravenatorsrobotics.common.operation.AutonomousMode;
-import com.bravenatorsrobotics.common.vision.TensorFlowObjectDetector;
 import com.bravenatorsrobotics.freightfrenzy.subsystem.IMUController;
 import com.bravenatorsrobotics.freightfrenzy.subsystem.LiftController;
-import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 
 @Autonomous(name="Autonomous")
 public class Auto extends AutonomousMode<MecanumDrive> {
@@ -44,6 +40,7 @@ public class Auto extends AutonomousMode<MecanumDrive> {
         turnTableSpinner = robot.GetMotor("turnTable", false);
         sideDistanceSensor = hardwareMap.get(RevColorSensorV3.class, "sideDistanceSensor");
 
+        liftController.SetCupPosition(LiftController.CupPosition.TILTED_POSITION);
     }
 
     @Override
@@ -57,7 +54,10 @@ public class Auto extends AutonomousMode<MecanumDrive> {
                 RunWarehouse();
                 break;
             case STORAGE_UNIT:
-                RunStorageUnit();
+                if(config.allianceColor == Config.AllianceColor.RED)
+                    RunRedStorageUnit();
+                else
+                    RunBlueStorageUnit();
                 break;
         }
     }
@@ -73,7 +73,7 @@ public class Auto extends AutonomousMode<MecanumDrive> {
     }
 
     // Storage Unit Code (compatible for both sides with the 'movementModifier')
-    private void RunStorageUnit() {
+    private void RunRedStorageUnit() {
         final double startAngle = imuController.GetZAxis();
 
         // Drive to the turn table
@@ -82,11 +82,6 @@ public class Auto extends AutonomousMode<MecanumDrive> {
         robot.drive.DriveByInches(0.25, 5);
 
         sleep(SLEEP_AMOUNT_MILLIS);
-
-        // 180 if blue
-        if(config.allianceColor == Config.AllianceColor.BLUE) {
-            robot.drive.TurnDegrees(0.25, 180, AbstractDrive.TurnDirection.CLOCKWISE);
-        }
 
         // TODO: Use Encoders
         // Strafe to the turn-table
@@ -105,30 +100,13 @@ public class Auto extends AutonomousMode<MecanumDrive> {
 
         // Turn Towards Turn-Table
         // TODO: Reverse
-        robot.drive.TurnDegrees(0.25,
-                config.allianceColor == Config.AllianceColor.RED ?
-                        15 :
-                        40,
-                config.allianceColor == Config.AllianceColor.RED ?
-                        AbstractDrive.TurnDirection.COUNTER_CLOCKWISE :
-                        AbstractDrive.TurnDirection.CLOCKWISE);
+        robot.drive.TurnDegrees(0.25, 15, AbstractDrive.TurnDirection.COUNTER_CLOCKWISE);
 
         sleep(SLEEP_AMOUNT_MILLIS);
 
-        // If blue strafe into turn-table
-//        if(config.allianceColor == Config.AllianceColor.BLUE) {
-//            robot.drive.Drive(0, 0.25, 0);
-//
-//            while(timer.seconds() < 0.25) { // Strafe Seconds
-//                if(!opModeIsActive()) break;
-//            }
-//
-//            robot.drive.Stop();
-//        }
-
         // Spin the turn-table
         // TODO: Use the config
-        turnTableSpinner.setPower(config.allianceColor == Config.AllianceColor.RED ? 1 : -1);
+        turnTableSpinner.setPower(1);
 
         timer.reset();
 
@@ -158,7 +136,7 @@ public class Auto extends AutonomousMode<MecanumDrive> {
         robot.drive.TurnDegrees(0.25, (int) Math.abs(imuCorrection) + 5, // 5 degree correction
                 imuCorrection > 0 ? AbstractDrive.TurnDirection.CLOCKWISE : AbstractDrive.TurnDirection.COUNTER_CLOCKWISE);
 
-        robot.drive.TurnDegrees(0.25, 90, AbstractDrive.TurnDirection.COUNTER_CLOCKWISE);
+        robot.drive.TurnDegrees(0.45, 90, AbstractDrive.TurnDirection.COUNTER_CLOCKWISE);
 
         sleep(SLEEP_AMOUNT_MILLIS);
 
@@ -168,7 +146,7 @@ public class Auto extends AutonomousMode<MecanumDrive> {
 
         robot.drive.Drive(0, -0.25, 0);
 
-        while(timer.seconds() < 0.94) { // Strafe Seconds
+        while(timer.seconds() < 1.05) { // Strafe Seconds
             if(!opModeIsActive()) break;
         }
 
@@ -178,7 +156,7 @@ public class Auto extends AutonomousMode<MecanumDrive> {
 
         robot.drive.Drive(-0.10, 0, 0);
 
-        final int distanceToShippingHub = 1415;
+        final int distanceToShippingHub = 1150;
         int startTicks = robot.GetDriveMotors()[0].getCurrentPosition();
 
         while(opModeIsActive()) {
@@ -187,7 +165,7 @@ public class Auto extends AutonomousMode<MecanumDrive> {
             telemetry.addData("Distance Sensor", distance);
             telemetry.update();
 
-            if(distance < 59.85 || Math.abs(robot.GetDriveMotors()[0].getCurrentPosition()) >= distanceToShippingHub)
+            if(distance < 59.85 || Math.abs(robot.GetDriveMotors()[0].getCurrentPosition() - startTicks) >= distanceToShippingHub)
                 break;
         }
 
@@ -203,7 +181,8 @@ public class Auto extends AutonomousMode<MecanumDrive> {
 
         sleep(SLEEP_AMOUNT_MILLIS);
 
-        robot.drive.TurnDegrees(0.25, 55, AbstractDrive.TurnDirection.COUNTER_CLOCKWISE); // Turn to shipping hub
+        final int degreesTowardsShippingHub = 63;
+        robot.drive.TurnDegrees(0.50, degreesTowardsShippingHub, AbstractDrive.TurnDirection.COUNTER_CLOCKWISE); // Turn to shipping hub
 
         sleep(SLEEP_AMOUNT_MILLIS);
 
@@ -221,24 +200,28 @@ public class Auto extends AutonomousMode<MecanumDrive> {
             liftController.GoToStage(LiftController.LiftStage.STAGE_2);
         }
 
-        robot.drive.DriveByInches(0.15, -5.0);
+        robot.drive.DriveByInches(0.15, -4.75); // Drive to shipping hub
 
-        while(liftController.GetLiftMotor().isBusy()) {
+        timer.reset();
+
+        while(liftController.GetLiftMotor().isBusy() && timer.seconds() < 1.25) {
             if(!opModeIsActive())
                 break;
         }
 
-        // TODO: Drop the block
-
+        // Drop the Block
+        liftController.SetCupPosition(LiftController.CupPosition.DUMPED_POSITION);
+        sleep(600);
+        liftController.SetCupPosition(LiftController.CupPosition.INTAKE_POSITION);
+        sleep(500);
         liftController.ZeroLift();
-
         sleep(SLEEP_AMOUNT_MILLIS);
 
-        robot.drive.DriveByInches(0.15, 15.0);
-
+        // Back up away from goal
+        robot.drive.DriveByInches(0.50, 15.0);
         sleep(SLEEP_AMOUNT_MILLIS);
 
-        robot.drive.TurnDegrees(0.25, 55, AbstractDrive.TurnDirection.CLOCKWISE); // Turn to shipping hub
+        robot.drive.TurnDegrees(0.25, degreesTowardsShippingHub, AbstractDrive.TurnDirection.CLOCKWISE); // Turn to shipping hub
 
         sleep(SLEEP_AMOUNT_MILLIS);
 
@@ -252,10 +235,81 @@ public class Auto extends AutonomousMode<MecanumDrive> {
 
         sleep(SLEEP_AMOUNT_MILLIS);
 
-        robot.drive.DriveByInches(0.5, -90);
+        robot.drive.DriveByInches(0.5, -85);
+    }
 
+    private void RunBlueStorageUnit() {
+        final double startAngle = imuController.GetZAxis();
 
+        // Drive to the turn table
 
+        // Drive off the wall
+        robot.drive.DriveByInches(0.25, 5);
+
+        sleep(SLEEP_AMOUNT_MILLIS);
+
+        robot.drive.TurnDegrees(0.5, 180, AbstractDrive.TurnDirection.CLOCKWISE);
+
+        sleep(SLEEP_AMOUNT_MILLIS);
+
+        // TODO: Use Encoders
+        // Strafe to the turn-table
+        ElapsedTime timer = new ElapsedTime();
+        timer.reset();
+
+        robot.drive.Drive(0, 0.25, 0);
+
+        while(timer.seconds() < 1.75) { // Strafe Seconds
+            if(!opModeIsActive()) break;
+        }
+
+        robot.drive.Stop();
+
+        // Turn towards turn-table
+        sleep(SLEEP_AMOUNT_MILLIS);
+        robot.drive.TurnDegrees(0.25, 55, AbstractDrive.TurnDirection.CLOCKWISE);
+        sleep(SLEEP_AMOUNT_MILLIS);
+
+        // Strafe into turn-table
+        robot.drive.Drive(0, 0.10, 0);
+        timer.reset();
+        while(timer.seconds() < 0.01) { // Strafe Seconds
+            if(!opModeIsActive()) break;
+        }
+        robot.drive.Stop();
+        sleep(SLEEP_AMOUNT_MILLIS);
+
+        // Spin turn-table
+        turnTableSpinner.setPower(-1);
+        timer.reset();
+        while(timer.seconds() < 2.5) { // Turn-Table
+            if(!opModeIsActive()) break;
+        }
+        turnTableSpinner.setPower(0);
+
+        // Strafe away from turn-table
+        sleep(SLEEP_AMOUNT_MILLIS);
+        timer.reset();
+        robot.drive.Drive(0, -0.25, 0);
+        while(timer.seconds() < 0.50) { // Strafe Seconds
+            if(!opModeIsActive()) break;
+        }
+        robot.drive.Stop();
+
+        // Straighten out
+        sleep(SLEEP_AMOUNT_MILLIS);
+        robot.drive.TurnDegrees(0.25, (int) (imuController.GetZAxis() - 90), AbstractDrive.TurnDirection.CLOCKWISE);
+
+        // Strafe to the block
+        sleep(SLEEP_AMOUNT_MILLIS);
+        timer.reset();
+        robot.drive.Drive(0, -0.12, 0);
+        while(timer.seconds() < 1.1) { // Strafe Seconds
+            if(!opModeIsActive()) break;
+        }
+        robot.drive.Stop();
+
+        sleep(SLEEP_AMOUNT_MILLIS);
     }
 
     @Override
