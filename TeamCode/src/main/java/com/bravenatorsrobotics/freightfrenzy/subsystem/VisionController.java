eca5ccdb5300b666efd89e9bccce03c3f9f538de
Nick Fanelli <nickfanelli5@gmail.com>
2022-01-23 10:07:57 -0500

    chore: add easyopencv dependency to gradle build

---
+++ TeamCode/src/main/java/com/bravenatorsrobotics/freightfrenzy/subsystem/VisionController.java
@@ -0,0 +1,11 @@
+package com.bravenatorsrobotics.freightfrenzy.subsystem;
+
+import com.bravenatorsrobotics.common.operation.OperationMode;
+
+public class VisionController extends AbstractController {
+
+    public VisionController(OperationMode<?> operationMode) {
+        super(operationMode);
+    }
+
+}
