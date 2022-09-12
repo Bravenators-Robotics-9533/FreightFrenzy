7893d4121fb0acb9932478a818f534f5be24c2fb
Nick Fanelli <nickfanelli5@gmail.com>
2022-09-12 17:50:22 -0400

    chore: remove last year code

--- TeamCode/src/main/java/com/bravenatorsrobotics/common/core/BravenatorRuntimeException.java
+++
@@ -1,9 +0,0 @@
-package com.bravenatorsrobotics.common.core;
-
-public class BravenatorRuntimeException extends RuntimeException {
-
-    public BravenatorRuntimeException(String exception) {
-        super(exception);
-    }
-
-}
